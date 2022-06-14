14611c6cc9d9ecf2679bf4626cc4d486913187b7
Dmytro Kovalchuk <dimasmith@gmail.com>
2022-06-15 02:16:04 +0300

    feat: style trainings start menu

--- lib/start_training_screen.dart
+++ lib/start_training_screen.dart
@@ -12,28 +12,66 @@ class StartTrainingScreen extends StatelessWidget {
           appBar: AppBar(
             title: const Text("Straight Pool Training"),
           ),
-          body: Center(
-            child: buildActions(trainings, context),
-          )
-      ),
+          body: ButtonMenu(children: [
+            buildActions(trainings, context),
+          ])),
     );
   }
 
-  ElevatedButton buildActions(Trainings trainings, BuildContext context) {
-    if (trainings.isRunning()) {
-      return ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, "/training");
-          },
-          child: const Text("Resume Training"));
-    }
-    return ElevatedButton(
-            child: const Text("Start Training"),
+  Widget buildActions(Trainings trainings, BuildContext context) {
+    return trainings.isRunning()
+        ? MenuButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/training');
+            },
+            child: const Text('Resume Training'),
+          )
+        : MenuButton(
             onPressed: () {
               trainings.start();
-              Navigator.pushNamed(context, "/training");
+              Navigator.pushNamed(context, '/training');
             },
+            child: const Text('Start Training'),
           );
   }
 }
 
+class ButtonMenu extends StatelessWidget {
+  final List<Widget> children;
+
+  const ButtonMenu({Key? key, required this.children}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: children,
+        ),
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  final VoidCallback? onPressed;
+  final Widget? child;
+
+  const MenuButton({Key? key, this.onPressed, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: child,
+        ),
+      ),
+    );
+  }
+}
